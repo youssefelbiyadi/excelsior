6d02d5a36b33a545e7085d48db4f3c93613ccc5b
youssefelbiyadi <50877390+youssefelbiyadi@users.noreply.github.com>
2024-01-30 12:24:57 +0100

    Update cp.py

--- cp.py
+++ cp.py
@@ -1,28 +1,33 @@
-def _refresh_cluster_upgrade_step(
-    self, cluster_upgrade: DataplaneClusterUpgrade
-) -> DataplaneClusterUpgrade:
-    cluster = self.repositories.dataplane_cluster.get_by_id(
-        cluster_upgrade.new_dataplane_cluster_id
-    )
+    @property
+    def state(self) -> ComponentUpgradeState:
+        if all(
+            [
+                self.db_creation_state == OperationState.SUCCESS,
+                self.db_refresh_state == OperationState.SUCCESS,
+                self.secret_update_state == OperationState.SUCCESS,
+                self.kube_restart_state == OperationState.SUCCESS,
+            ]
+        ):
+            state = ComponentUpgradeState.UPGRADED
+        elif any(
+            [
+                self.db_creation_state == OperationState.FAILED,
+                self.db_refresh_state == OperationState.FAILED,
+                self.secret_update_state == OperationState.FAILED,
+                self.kube_restart_state == OperationState.FAILED,
+            ]
+        ):
+            state = ComponentUpgradeState.UPGRADE_FAILED
+        elif all(
+            [
+                self.db_creation_state == OperationState.PENDING,
+                self.db_refresh_state == OperationState.PENDING,
+                self.secret_update_state == OperationState.PENDING,
+                self.kube_restart_state == OperationState.PENDING,
+            ]
+        ):
+            state = ComponentUpgradeState.UPGRADING
+        else:
+            state = ComponentUpgradeState.UPGRADE_REQUESTED
 
-    new_upgrade_step = None
-
-    if self._new_cluster_is_active(cluster.id):
-        new_upgrade_step = DataplaneClusterUpgradeStep.NEW_CLUSTER_CREATED
-
-    if cluster.status == Status.CREATING:
-        new_upgrade_step = DataplaneClusterUpgradeStep.NEW_CLUSTER_CREATING
-
-    if cluster.status == Status.FAILED:
-        new_upgrade_step = DataplaneClusterUpgradeStep.NEW_CLUSTER_CREATION_FAILED
-
-    if (
-        new_upgrade_step
-        and cluster_upgrade.upgrade_step != new_upgrade_step
-    ):
-        self._update_cluster_upgrade_with(
-            cluster_upgrade,
-            upgrade_step=new_upgrade_step,
-        )
-
-    return self.repositories.dataplane_cluster_upgrade.get_by_id(cluster_upgrade.id)
+        return state
